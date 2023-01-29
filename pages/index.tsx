fa50b9bce67a95466366b36216dec7038e47bd6c
Anshika <anshikathakur596@gmail.com>
2023-01-29 23:59:27 +0530

    Landing tsx called

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import Landing from './Landing'
 export default function IndexPage() {
   return (
-    <div>
-      Hello World. <Link href="/about">About</Link>
-      <Image src="/images/profile.jpg" alt='' width={200} height={200} />
-    </div>
+    <>
+    <Landing/>
+    </>
   )
 }
