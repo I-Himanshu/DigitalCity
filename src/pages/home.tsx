1078bb3f778a788d6705460643f0496be0651ffb
Anshika <anshikathakur596@gmail.com>
2023-01-30 01:58:09 +0530

    navitems added

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <main className="h-screen flex flex-col justify-center w-full bg-cover bg-white px-[10vmin] lg:px-[20vmin] bg-[url(/Home/Mobilebackground.png)] lg:bg-[url(/Home/Background.svg)]">
+    <main className="h-screen z-10 flex flex-col justify-center w-full bg-cover bg-white px-[10vmin] lg:px-[20vmin] bg-[url(/Home/Mobilebackground.png)] lg:bg-[url(/Home/Background.svg)]">
       <p className="font-bold text-[#3852CE] text-[8vmin] mt-[30vmin] lg:mt-0">Digital TownHall</p>
       <p className="font-bold text-[#000] text-[6vmin] leading-[5.1vmin]">Modern people, Modern town</p>
       <p className="text-[#aaa] text-[2.5vmin] mt-[5vmin] lg:max-w-[35vw] tracking-[0.2vmin]">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora doloribus iusto voluptatibus expedita dicta dolore molestias perferendis, voluptatum unde assumenda eos reprehenderit culpa ipsam similique fugit quas rem ea laborum?</p>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Home from './home'
 import '../../styles/globals.css'
+import Navbar from 'components/Navbar'
 type Props = {}
 
-export default function index({}: Props) {
-  return (
-    <>
-        <Home/>
-    </>
-  )
+export default function index({ }: Props) {
+    return (
+        <>
+            <div className="relative flex flex-col">
+                <Navbar />
+                <Home />
+            </div>
+        </>
+    )
 }
