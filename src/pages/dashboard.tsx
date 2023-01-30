002e04322f7ced4c4cc7f7276d957cb0f3146e84
I-sandesh <83366556+I-Himanshu@users.noreply.github.com>
2023-01-30 22:15:56 +0530

    adding

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function dashboard() {
+  return (
+    <div>
+        Hey Bro
+    </div>
+  )
+}
+
+export default dashboard
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 
 export default function Home() {
@@ -6,7 +7,8 @@ export default function Home() {
       <p className="font-bold text-[#3852CE] text-[8vmin] mt-[30vmin] lg:mt-0">Digital TownHall</p>
       <p className="font-bold text-[#000] text-[6vmin] leading-[5.1vmin]">Modern people, Modern town</p>
       <p className="text-[#aaa] text-[2.5vmin] mt-[5vmin] lg:max-w-[35vw] tracking-[0.2vmin]">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora doloribus iusto voluptatibus expedita dicta dolore molestias perferendis, voluptatum unde assumenda eos reprehenderit culpa ipsam similique fugit quas rem ea laborum?</p>
-      <button className="bg-[#3852ce] rounded-sm shadow-sm text-white text-[3.5vmin] transition-all hover:bg-[#3852ce]/80 p-[2vmin] max-w-[30vmin] mt-[5vmin]">Get Started</button>
+      <Link href="/dashboard" className="bg-[#3852ce] rounded-sm shadow-sm text-white text-[3.5vmin] transition-all hover:bg-[#3852ce]/80 p-[2vmin] max-w-[30vmin] mt-[5vmin]">Get Started</Link>
+      {/* <a href="https://www.google.com/" className="bg-[#3852ce] rounded-sm shadow-sm text-white text-[3.5vmin] transition-all hover:bg-[#3852ce]/80 p-[2vmin] max-w-[30vmin] mt-[5vmin]">Get Started</a> */}
     </main>
   )
 }
