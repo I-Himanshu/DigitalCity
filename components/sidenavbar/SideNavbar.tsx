fd7b5845c68cd55230dbd310aa60b4f5c3d5ab3d
Anshika <anshikathakur596@gmail.com>
2023-02-02 23:07:40 +0530

    Update SideNavbar.tsx

--- components/sidenavbar/SideNavbar.tsx
+++ components/sidenavbar/SideNavbar.tsx
@@ -6,7 +6,7 @@ function SideNavbar() {
             <div className="w-[20%] h-[100vh] shadow-md px-[1vmin] bg-[#3852CE]/70 ">
                 <ul className="relative">
                     <li className="relative">
-                        <a className="flex items-center text-[2.7vmin] mt-[2vmin] py-[1.2vmin] px-[1.8vmin] h-[8vmin] overflow-hidden text-white text-ellipsis whitespace-nowrap rounded hover:text-[#3852ce] font-medium hover:bg-gray-100 transition duration-300 ease-in-out" href="#!"> <i className="fa-solid fa-cloud-sun text-[2.7vmin] ml-[2vmin]"></i> Weather</a>
+                        <a className="flex items-center text-[2.7vmin] mt-[2vmin] py-[1.2vmin] px-[1.8vmin] h-[8vmin] overflow-hidden text-white text-ellipsis whitespace-nowrap rounded hover:text-[#3852ce] font-medium hover:bg-gray-100 transition duration-300 ease-in-out" href="#!"> <i className="fa-solid fa-cloud-sun text-[2.7vmin] mr-[2vmin]"></i> Weather</a>
                     </li>
                     <li className="relative">
                         <a className="flex items-center text-[2.7vmin] mt-[2vmin] py-[1.2vmin] px-[1.8vmin] h-[8vmin] overflow-hidden text-white text-ellipsis whitespace-nowrap rounded hover:text-[#3852ce] font-medium hover:bg-gray-100 transition duration-300 ease-in-out" href="#!">Local News</a>
